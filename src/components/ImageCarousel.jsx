22db2b311d3a8aa8ff5e393fa9bbed128db5c51d
Nemes Tamás - CAD1 <nemestamas94@gmail.com>
2026-04-21 17:57:38 +0200

    Implement ImageCarousel component with styling and image selection functionality

--- src/components/ImageCarousel.jsx
+++ src/components/ImageCarousel.jsx
@@ -1,6 +1,24 @@
+import { useState } from "react"
+
 const ImageCarousel = () => {
+  const images = ["image-product-1","image-product-2","image-product-3","image-product-4"]
+  const [currentIdx, setCurrentIdx] = useState(0)
+
   return (
-    <div>ImageCarousel</div>
+    <div className="imageCarousel">
+        <img src={`/images/${images[currentIdx]}.jpg`} alt="Selected Image" />
+
+        <div className="imageSelector">
+        {   images.map((image, idx) => 
+            <button 
+                disabled={idx === currentIdx} 
+                onClick={()=>setCurrentIdx(idx)} 
+            >
+                <img src={`/images/${image}-thumbnail.jpg`} />
+            </button> 
+        ) }
+        </div>
+    </div>
   )
 }
 
